8772df4bfb4346420376990c1520827b51a1677c
JessicaEulaers <33091372+JessicaEulaers@users.noreply.github.com>
2018-04-18 14:37:31 +0200

    Add files via upload

--- ExamenJava2Vraag1/SPRING-EXAMEN-JAVA2-VRAAG1-START/src/main/java/edu/ap/spring/view/UI.java
+++ ExamenJava2Vraag1/SPRING-EXAMEN-JAVA2-VRAAG1-START/src/main/java/edu/ap/spring/view/UI.java
@@ -1,11 +1,76 @@
 package edu.ap.spring.view;
 
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.beans.EventHandler;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.TransferHandler;
+
+
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UI implements InitializingBean {
 	
+	@Autowired
+	EventHandler eventHandler;
+
+	private JFrame jFrame;
+	private JLabel label1, label2;
+	private JTextField input;
+	private JPanel controlPanel;
+    private JButton OK;
+    
+    public UI() {}
+    
+    public void setupUI() {
+	    	jFrame = new JFrame("Spring 8ball");
+	    	jFrame.setLayout(new FlowLayout());
+	    	
+	    	controlPanel = new JPanel();
+	    controlPanel.setLayout(new GridLayout(3, 2));
+
+		label1 = new JLabel("input : ");
+		input = new JTextField(15);
+		input.setDragEnabled(true);
+		
+		OK = new JButton();
+		OK.setText("ok");
+		OK.setTransferHandler(new TransferHandler("text"));
+
+		controlPanel.add(label1);
+		controlPanel.add(input);
+		controlPanel.add(label2);
+
+		controlPanel.add(OK);
+
+		jFrame.add(controlPanel);
+		        
+		jFrame.setSize(400, 400);
+		jFrame.setLocationRelativeTo(null);
+		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jFrame.pack();
+		jFrame.setVisible(true);
+    }
+
+    public JFrame getjFrame() {
+        return this.jFrame;
+    }
+    
+    public JTextField getinput() {
+		return this.input;
+}
+
+    public JButton getButton() {
+        return OK;
+    }
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		System.setProperty("java.awt.headless", "false");
---
+++ ExamenJava2Vraag2/SPRING-EXAMEN-JAVA2-VRAAG2-START/src/main/java/edu/ap/spring/Exam/Exam.java
@@ -0,0 +1,48 @@
+package edu.ap.spring;
+
+import java.awt.Point;
+import java.util.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Exam {
+	
+	// Maak gebruik van lambdas en streams om een array met alle
+	// priemgetallen terug te geven
+	// 2 punten
+	public int[] getPrimes(int[] numbers) {
+		int n = numbers.length;
+		int[] priem = new int[n];
+		
+		for(int i = 2; i < n ; i++){
+		    priem
+		}
+		 
+		return priem;
+	}
+	
+	// Maak gebruik van lambdas en streams alle lowercase characters
+	// te tellen in de gegeven string
+	// 1 punt
+	public int countLowercaseCharacters(String string) {
+		int count = countLowercaseCharacters(string);
+		return count;
+        
+	}
+	
+	// Maak gebruik van lambdas en streams om de som van alle
+	// x-coordinaten uit de lijst van points te berekenen
+	// 1 punt
+	public int sumOfX(List<Point> points) {
+		
+		
+	}
+	
+	// Maak gebruik van lambdas en streams om een comma-separated
+	// string te maken die alle x-coordinaten bevat die groter of gelijk 
+	// zijn aan twee
+	// 2 punten
+	public String getXOverTwo(List<Point> points) {
+		
+	}
+}
